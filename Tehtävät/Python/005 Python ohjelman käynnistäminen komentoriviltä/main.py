864929b5c3745036c44f1dece7cd41d36c521b8e
sievala <aleksi@sievala.dev>
2022-02-11 10:44:13 +0200

    Added 005.

---
+++ Tehtävät/Python/005 Python ohjelman käynnistäminen komentoriviltä/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 005 Python ohjelman käynnistäminen komentoriviltä
+
+from time import sleep
+
+print("Käynnistit ohjelman onnistuneesti!")
+sleep(1)
+print("...")
+sleep(1)
+print("Voit siirtyä seuraavaan tehtävään.")
