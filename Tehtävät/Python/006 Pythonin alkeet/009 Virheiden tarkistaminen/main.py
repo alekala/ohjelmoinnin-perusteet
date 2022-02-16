148ec9967e871d66b1f2b5df6d3ef21e3161ef16
sievala <aleksi@sievala.dev>
2022-02-16 10:02:53 +0200

    Added 009.

--- Tehtävät/Python/006 Pythonin alkeet/009 Virheiden tarkistaminen/main.py
+++ Tehtävät/Python/006 Pythonin alkeet/009 Virheiden tarkistaminen/main.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 # 009 Virheiden tarkistaminen
+
+cat_count = 10
+cat_names = ["Muffin", "Paws", "Whiskers"]
+
+# Alkaen riviltä 8, yritä miinustaa cat_count numero cat_names listasta ja tee virheiden tarkastus.
+# Mikäli lauseke tuottaa TypeError virheen, tulosta "Kissojen määrää ei voi miinustaa kissojen nimistä!".
+
