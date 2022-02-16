f1f591bf4a1d1707f51faf102563d6cc9f9244a2
sievala <aleksi@sievala.dev>
2022-02-16 09:31:31 +0200

    Added 007 and 008.

--- Tehtävät/Python/006 Pythonin alkeet/007 Kommentointi/main.py
+++ Tehtävät/Python/006 Pythonin alkeet/007 Kommentointi/main.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 # 007 Kommentointi
+
+# Lisää riville 5 yksirivinen kommentti, johon jätät viestin opettajalle.
+
+
+# Alkaen riviltä 8, lisää monirivinen kommentti jossa on haluamaasi tekstiä esim. Lorem Ipsumia.
+
--- Tehtävät/Python/006 Pythonin alkeet/008 for ja while/main.py
+++ Tehtävät/Python/006 Pythonin alkeet/008 for ja while/main.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 # 008 for ja while
+
+value = 0
+
+animals = ["Cat", "Dog", "Parrot", "Squirrel", "Sheep"]
+
+# Alkaen riviltä 12, kirjoita while silmukka joka toistaa itseään, kunnes value muttujan arvo on 10.
+# Jokaisella toistolla:
+# - Plussaa value arvoon yksi.
+# - Tulosta valuen arvo.
+
+
+# Alkaen riviltä 15, kirjoita for silmukka joka tulostaa kaikki numerot 1-10.
+
+
+# Alkaen riviltä 18, kirjoita for silmukka joka tulostaa kaikki animals listan osat.
+
