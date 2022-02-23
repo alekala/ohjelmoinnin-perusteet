8d9d81f7f427a7e19a3bb9ce02131a9329a31aed
sievala <aleksi@sievala.dev>
2022-02-23 07:53:15 +0200

    Added 011 and 012.

--- Tehtävät/Python/006 Pythonin alkeet/011 Tiedoston avaaminen ja lukeminen/main.py
+++ Tehtävät/Python/006 Pythonin alkeet/011 Tiedoston avaaminen ja lukeminen/main.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
 # 011 Tiedoston avaaminen ja lukeminen
+
+# Rivistä 5 eteenpäin, käytä open funktiota message.txt tiedoston avaamiseen ja tulosta sen sisältö.
+
--- Tehtävät/Python/006 Pythonin alkeet/012 Tiedostoon kirjoittaminen/main.py
+++ Tehtävät/Python/006 Pythonin alkeet/012 Tiedostoon kirjoittaminen/main.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
 # 012 Tiedostoon kirjoittaminen
+
+# Rivistä 5 eteenpäin, käytä open funktiota message.txt tiedoston avaamiseen ja kirjoita sen sisälle haluamasi viesti.
+
